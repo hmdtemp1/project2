408835bca91d41bb885df7afd668947217c0343f
unknown <NehalPatel@Room206.sdjic.com>
2018-09-08 11:36:01 +0530

    bootstrap added

--- header.php
+++ header.php
@@ -11,10 +11,10 @@ if(isset($userData)){
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="../bootstrap/dist/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css"/>
 	<link rel="stylesheet" href="style.css"/>
-	<script src="../jquery3.js"></script>
-	<script src="../bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="./jquery3.js"></script>
+	<script src="./bootstrap/js/bootstrap.min.js"></script>
 		
 	</head>
 	<body>
